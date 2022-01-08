a93a207b1dffe26d334a7054951d97c92cd3ae3e
grigmily <grigmily@gmail.com>
2022-01-08 21:46:51 +0300

    init: task solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function toReadable (number) {
-  
-}
+    let names = ['zero', 'one', 'two', 'three','four','five','six','seven','eight','nine','ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen','eighteen','nineteen',''];
+    let decade = ['twenty', 'thirty', 'forty','fifty','sixty','seventy','eighty','ninety'];
+    if (number < 20) return (names[number]);
+    else if (number < 100) return ((number % 10) ? (decade[(Math.floor(number/10)-2)]+' '+names[(number % 10)||20]) : decade[(Math.floor(number/10)-2)]);
+    else if (number < 1000) return ((number % 100) ? (names[Math.floor(number/100)])+' hundred ' + (toReadable(number % 100)) : ((names[Math.floor(number/100)])+' hundred'));
+}   
